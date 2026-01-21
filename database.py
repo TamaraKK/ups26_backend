aeb060c245a312d61ffbffa4ba438ec395a10586
TamaraKK <tomkavins@gmail.com>
2026-01-21 18:29:10 +0500

    dockercompose - hive, loki, postgres, prometheus

--- database.py
+++ database.py
@@ -1,3 +1,24 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
+
+
+
+
+
+
+
+
+
 # from sqlalchemy import create_engine
 # from sqlalchemy.orm import sessionmaker, declarative_base
 
@@ -7,13 +28,19 @@
 # Sessionlocal = sessionmaker(bind=engine, autoflush=False, autocommit = False)
 # Base = declarative_base()
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-Base = declarative_base()
+
+
+
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+# Base = declarative_base()
 
 
