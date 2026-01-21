5b5377543a5c8f583d883bd46374e8ba0a1f210c
TamaraKK <tomkavins@gmail.com>
2026-01-21 11:51:54 +0500

    cors

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import groups, devices
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="IoT Manager API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(groups.router)
 app.include_router(devices.router)
